493d9c99b22e6bd26e84ecaa0c9e6f27175ebb6b
IvanAnikin <ivan.anikin@outlook.com>
2022-01-06 15:01:49 +0100

    :)

--- StockG/Testing/stable_baselines.py
+++
@@ -1,5 +0,0 @@
-from stable_baselines import PPO2
-
-model = PPO2.load("E:/VS_Projects/Deep-Reinforcement-Learning-for-Automated-Stock-Trading-Ensemble-Strategy-ICAIF-2020/trained_models/PPO_model")
-
-print(model)
--- StockG/Testing/trainers_testing2.py
+++
@@ -1,6 +0,0 @@
-
-from StockG.Agents.Trader import technical
-
-best, log_string, Return, signals_img, portfolio_img = technical.get_signals_loop()
-
-print(best)
